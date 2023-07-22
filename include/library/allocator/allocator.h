b6c3926e8f4c5831b25e5caa57b6ea4de298b058
Khalil Henoud <khalilhenoud@gmail.com>
2023-07-22 17:49:52 -0400

    first commit

---
+++ include/library/allocator/allocator.h
@@ -0,0 +1,24 @@
+/**
+ * @file allocator.h
+ * @author khalilhenoud@gmail.com
+ * @brief 
+ * @version 0.1
+ * @date 2023-01-02
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#ifndef ALLOCATOR_H
+#define ALLOCATOR_H
+
+
+typedef 
+struct allocator_t {
+  void* (*mem_alloc)(size_t size);
+  void (*mem_free)(void *ptr);
+  void* (*mem_realloc)(void *ptr, size_t new_size);
+  void* (*mem_cont_alloc)(size_t num, size_t size);
+  void* (*mem_alloc_alligned)(size_t alignment, size_t size);
+} allocator_t;
+
+#endif
---
+++ include/library/string/fixed_string.h
@@ -0,0 +1,20 @@
+/**
+ * @file fixed_string.h
+ * @author khalilhenoud@gmail.com
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-26
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#ifndef FIXED_STRING_H
+#define FIXED_STRING_H
+
+
+typedef
+struct fixed_str_t {
+  char data[512];
+} fixed_str_t;
+
+#endif
