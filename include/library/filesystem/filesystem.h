54d802b8f178b180e25269753cf3430db88738f4
Khalil Henoud <khalilhenoud@gmail.com>
2024-01-06 14:34:33 -0500

    directory query and string refactor.

---
+++ include/library/filesystem/filesystem.h
@@ -0,0 +1,46 @@
+/**
+ * @file filesystem.h
+ * @author khalilhenoud@gmail.com
+ * @brief 
+ * @version 0.1
+ * @date 2024-01-05
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#ifndef LIB_FILESYSTEM_H
+#define LIB_FILESYSTEM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#define MAX_PATH_LENGTH          512
+#define MAX_DIR_ENTRIES          512
+
+
+// TODO: support utf8 encoding.
+typedef struct allocator_t allocator_t;
+
+typedef char dir_name_t[MAX_PATH_LENGTH];
+
+typedef
+struct dir_entries_t {
+  dir_name_t dir_names[MAX_DIR_ENTRIES];
+  uint32_t used;
+} dir_entries_t;
+
+void
+get_subdirectories(
+  const char* directory,
+  dir_entries_t* entries);
+
+#include "filesystem.impl"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- include/library/string/string.h
+++ include/library/string/string.h
@@ -11,84 +11,59 @@
 #ifndef LIB_STRING_H
 #define LIB_STRING_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdint.h>
-#include <string.h>
-#include <assert.h>
-#include <library/allocator/allocator.h>
 
 
+// TODO: how this functionality between stack and heap is a bit of a problem. I
+// need to address this sooner or later. This issue will re-occur with the
+// different library containers implementation, I need a good solution for this
+// system wide.
+
+typedef struct allocator_t allocator_t;
+
+/**
+ * @brief holds a null terminated string and the allocator that was used to
+ * create the instance and the buffer within it.
+ * we cache the size of the string.
+ * NOTE: currently there is no option to specify different allocators for the
+ * instance and the internal buffer, so if you are using the type on stack use
+ * clear_string() instead of free_string().
+ */
 typedef
 struct string_t {
   char* str;
-  // the size of str without the null terminator.
   uint32_t size;
-  // the allocator used to allocate the string.
   const allocator_t* allocator;
 } string_t;
 
-// TODO: expand the functionality provided here.
-// TODO: implement a string builder to avoid constant reallocation.
 
-inline
-string_t*
-create_string(const char* str, const allocator_t* allocator)
-{
-  string_t* string = (string_t*)allocator->mem_alloc(sizeof(string_t));
-  assert(string);
-  string->allocator = allocator;
-  string->size = strlen(str);
-  string->str = (char*)allocator->mem_alloc(string->size + 1);
-  memcpy(string->str, str, string->size + 1);
-  return string;
-}
+// allocates the string instance on the heap, reuses the allocator to allocate
+// the internal buffer.
+string_t* allocate_string(const char* str, const allocator_t* allocator);
 
-// NOTE: do not use string after this operation.
-inline
-void
-free_string(string_t* string)
-{
-  if (string->str)
-    string->allocator->mem_free(string->str);
-  string->allocator->mem_free(string);
-}
+// frees the dynamically allocated string_t instead, do not use after this.
+void free_string(string_t* string);
 
-// returns the previous size, preserves the allocator.
-inline
-uint32_t
-clear_string(string_t* string)
-{
-  uint32_t previous_size = string->size;
-  string->size = 0;
-  if (string->str)
-    string->allocator->mem_free(string->str);
-  string->str = NULL;
-  return previous_size;
-}
+// clears the string, return the previous size, preserves the allocator.
+uint32_t clear_string(string_t* string);
 
-inline
-uint32_t
-assign_string(string_t* string, const char* str)
-{
-  uint32_t previous_size = clear_string(string);
-  string->size = strlen(str);
-  string->str = (char*)string->allocator->mem_alloc(string->size + 1);
-  memcpy(string->str, str, string->size + 1);
-}
+// assigns str to string and returns the previous string size.
+uint32_t assign_string(string_t* string, const char* str);
 
-// NOTE: copies the string data but keeps the original allocator
-inline
-void
-copy_string(string_t* target, const string_t* source)
-{
-  clear_string(target);
-  assign_string(target, source->str);
-}
+// copies the string data but keeps the original allocator
+void copy_string(string_t* target, const string_t* source);
 
-inline
-int32_t
-string_equal(const string_t* left, const string_t* right)
-{
-  return !strcmp(left->str, right->str);
+// returns 1 if equal 0 otherwise.
+int32_t string_equal(const string_t* left, const string_t* right);
+
+#include "string.impl"
+
+#ifdef __cplusplus
 }
+#endif
 
 #endif
